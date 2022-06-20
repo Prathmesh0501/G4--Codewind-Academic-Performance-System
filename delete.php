f318b1cc4b4b6a527dfc2ae2cd2e2fd5ac1786b6
aerafshaikh <107240720+aerafshaikh@users.noreply.github.com>
2022-06-20 17:54:01 +0530

    Add files via upload

---
+++ delete.php
@@ -0,0 +1,33 @@
+<?php
+if(isset($_GET['id']))
+{
+	$db = mysqli_connect('localhost','root','','apes');
+	 function validate($data){
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+	}
+
+	$id = validate($_GET['id']);
+
+	$sql = "DELETE FROM teacher_data
+	        WHERE id=$id";
+   $result = mysqli_query($db, $sql);
+   if ($result) {
+   	  header("Location: teacher_list.php?success=successfully deleted");
+	  echo("successfully deleted");
+   }else {
+      header("Location: teacher_list.php?error=unknown error occurred&$user_data");
+   }
+
+}
+else
+{
+	header("Location: teacher_list.php");
+}
+
+
+
+
+?>
---
+++ fy_stud_add_data.php
@@ -0,0 +1,7 @@
+<?php 
+
+$db_user = "root";
+$db_pass = "";
+$db_name = "apes";
+
+$db = mysqli_connect("localhost","root","","apes");
