ca9736eb2c7a1ace0dbaeafe615b39882885e877
aerafshaikh <107240720+aerafshaikh@users.noreply.github.com>
2022-06-21 13:21:58 +0530

    Delete htmls directory

--- htmls/fy_stud_add_data.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-$db_user = "root";
-$db_pass = "";
-$db_name = "fy_stud";
-
-$db = mysqli_connect("localhost","root","","fy_stud");
--- htmls/sy_student_list_php.php
+++
@@ -1,8 +0,0 @@
-<?php
-$db = mysqli_connect('localhost','root','','fy_stud');
-
-$sql = "SELECT * FROM sy_stud_data ORDER BY first_name ";
-
-$result = mysqli_query($db,$sql);
-
-
